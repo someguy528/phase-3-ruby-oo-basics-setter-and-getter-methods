a0e63617ffb2408a6a204db3a91456466b9378df
Brandon Castro <castroyou999@gmail.com>
2023-01-19 19:01:53 -0500

    commit tree bc801e168a74c35bfaf21b1f2e4fa75acf85b957

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
 
+    def name=(persons_name)
+        @name = persons_name
+    end
+    def job=(persons_job)
+        @job = persons_job
+    end
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 end
